0ea722e1867cb26fafce401257ca06616fb5d197
snaulX <alexandergunger666@gmail.com>
2020-10-08 18:44:03 +0300

    Start creating game

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movement;
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            movement = new Vector3(1, 0);
+        }
+        else
+        {
+            movement = new Vector3();
+        }
+        transform.Translate(movement);
+    }
+}
